366c870f1c3376c38440720cd6877a0d290e0233
Sean Briggs <briggs.sea0642@stu.stech.edu>
2025-12-12 13:15:30 -0700

    Chapter 3 Tutorial

---
+++ Bob's Auto Parts/array_test.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Array Testing</title>
+</head>
+<body>
+<?php
+
+$categories = array(array(array('CAR_TIR', 'Tires', 100),
+						  array('CAR_OIL', 'Oil', 10),
+						  array('CAR_SPK', 'Spark Plugs', 4)),
+					array(array('VAN_TIR', 'Tires', 120),
+						  array('VAN_OIL', 'Oil', 12),
+						  array('VAN_SPK', 'Spark Plugs', 5)),
+					array(array('TRK_TIR', 'Tires', 150),
+						  array('TRK_OIL', 'Oil', 15),
+						  array('TRK_SPK', 'Spark Plugs', 6)),
+);
+
+for ($layer = 0; $layer < 3; $layer++) {
+	echo 'Layer'.$layer."<br />";
+	for ($row = 0; $row < 3; $row++) {
+		for ($column = 0; $column < 3; $column++) {
+			echo '|'.$categories[$layer][$row][$column];
+		}
+		echo '|<br />';
+	}
+}
+
+?>
+</body>
+</html>
