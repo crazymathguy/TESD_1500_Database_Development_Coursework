6b2aa19f6feb284dee317e0d76f320b01856f25d
Sean Briggs <briggs.sea0642@stu.stech.edu>
2026-01-20 14:50:51 -0700

    Chapter 11 Tutorial

--- Book o' Rama/books.sql
+++ Book o' Rama/books.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 16, 2026 at 08:51 PM
+-- Generation Time: Jan 20, 2026 at 09:48 PM
 -- Server version: 5.7.24
 -- PHP Version: 8.3.1
 
